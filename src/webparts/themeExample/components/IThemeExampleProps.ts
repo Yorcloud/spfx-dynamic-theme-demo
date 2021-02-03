96d5010af06856f22da65755cad6d56b62eb7d88
Rob Pearmain <rob@ipear.co.uk>
2021-02-03 13:09:42 +0000

    Initial Commit

---
+++ src/webparts/themeExample/components/IThemeExampleProps.ts
@@ -0,0 +1,3 @@
+export interface IThemeExampleProps {
+  description: string;
+}
---
+++ src/webparts/themeExample/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IThemeExampleWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ThemeExampleWebPartStrings' {
+  const strings: IThemeExampleWebPartStrings;
+  export = strings;
+}
